1c558d811bd1cdbc9f675f3d2790185e291a981f
victor.journe <victor.journe@interieur.gouv.fr>
2020-10-12 11:22:19 +0200

    Add git config

--- heat-templates/config-scripts/launch.sh
+++ heat-templates/config-scripts/launch.sh
@@ -5,11 +5,11 @@ docker network create dev
 # install locally postgres
 git clone https://github.com/datalab-mi/docker-postgres.git
 cd docker-postgres && docker-compose up -d && cd -
-docker network connect dev postgres
+docker network connect  postgres dev
 # install locally DSS
 git clone https://github.com/datalab-mi/docker-dss.git
 cd docker-dss && docker-compose up -d && cd -
-docker network connect dev dss
+docker network connect  dss dev
 
 echo "**************"
 echo "Start Services"
