b564d93c25eb0768c2c51e9d8bece39e4d74452d
victor journé <journe.victor@gmail.com>
2023-01-20 13:55:33 +0100

    :package: Update dsfr

--- heat-templates/params/get_token.sh
+++ heat-templates/params/get_token.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-[ -z "$OS_STORAGE_URL" -o -z "$OS_AUTH_URL" -o -z "$OS_USERNAME" -o -z "$OS_PROJECT_DOMAIN_NAME" -o -z "$OS_PASSWORD" ] && { echo "variable OS_* introuvable" ; exit 1 ; }
+[ -z "$OS_AUTH_URL" -o -z "$OS_USERNAME" -o -z "$OS_PASSWORD" ] && { echo "variable OS_* introuvable" ; exit 1 ; }
 
 curl_opt="--retry 10 --retry-delay 5 --retry-max-time 60 --connect-timeout 10"
 
 curl $curl_opt -s \
-	--request POST $OS_AUTH_URL/auth/tokens?nocatalog \
+	--request POST $OS_AUTH_URL/v3/auth/tokens?nocatalog \
 	--include \
 	--header "Content-Type: application/json" \
 	--data '{
